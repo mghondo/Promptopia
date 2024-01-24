8bebefe9410a2f3a9a3cbd5fd97a7dc5ac02fd6a
Morgan Hondros <morganhondros@gmail.com>
2024-01-24 18:16:39 -0500

    starting to use tailwind and had our first npm run dev.

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+<section className='w-full flex-center flex-col'>
+    <h2 className='head_text text-center'>Discover & Share</h2>
+</section>
+  )
+}
+
+export default Home
